16c7a340b8f650e9a5e0d337f952b08438771445
Saodat <saodat.riskiyeva@gmail.com>
2024-06-12 21:05:02 -0400

    converting and checking numbers

--- starter/script.js
+++ starter/script.js
@@ -251,3 +251,29 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+
+console.log(23 === 23.0000000000000009);
+console.log(0.1 + 0.2);
+
+console.log(Number('23'));
+console.log(+'23');
+
+console.log(Number.parseInt('30px', 10));
+
+console.log(Number.parseInt('2.5rem', 10));
+console.log(Number.parseFloat('2.5rem', 10));
+
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(20));
+
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20px'));
+console.log(Number.isFinite(20 / 0));
+
+console.log(Number.isInteger(20));
+console.log(Number.isInteger('20'));
+console.log(Number.isInteger(+'20px'));
+console.log(Number.isInteger(20 / 0));
