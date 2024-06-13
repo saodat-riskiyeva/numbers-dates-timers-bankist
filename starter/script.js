bfaca28c81e53f22d3c1795c86ef141492be86cb
Saodat <saodat.riskiyeva@gmail.com>
2024-06-12 21:24:19 -0400

    Math and rounding

--- starter/script.js
+++ starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,28 +252,76 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(23 === 23.0);
+// console.log(23 === 23.0);
 
-console.log(23 === 23.0000000000000009);
-console.log(0.1 + 0.2);
+// console.log(23 === 23.0000000000000009);
+// console.log(0.1 + 0.2);
 
-console.log(Number('23'));
-console.log(+'23');
+// console.log(Number('23'));
+// console.log(+'23');
 
-console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('30px', 10));
 
-console.log(Number.parseInt('2.5rem', 10));
-console.log(Number.parseFloat('2.5rem', 10));
+// console.log(Number.parseInt('2.5rem', 10));
+// console.log(Number.parseFloat('2.5rem', 10));
 
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(20));
 
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20px'));
-console.log(Number.isFinite(20 / 0));
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20px'));
+// console.log(Number.isFinite(20 / 0));
 
-console.log(Number.isInteger(20));
-console.log(Number.isInteger('20'));
-console.log(Number.isInteger(+'20px'));
-console.log(Number.isInteger(20 / 0));
+// console.log(Number.isInteger(20));
+// console.log(Number.isInteger('20'));
+// console.log(Number.isInteger(+'20px'));
+// console.log(Number.isInteger(20 / 0));
+
+// console.log(Math.sqrt(25)); // square root
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+// console.log(32 ** (1 / 5));
+
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, '23', 11, 2));
+// console.log(Math.max(5, 18, '23rem', 11, 2));
+
+// console.log(Math.min(5, 18, 23, 11, 2));
+// console.log(Math.min(5, 18, '23', 11, 2));
+// console.log(Math.min(5, 18, '23rem', 11, 2));
+
+// console.log(Math.PI);
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(120, 517));
+console.log(randomInt(1232, 8371));
+
+// console.log(Math.trunc(23.3));
+// console.log(Math.trunc(23.9));
+
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
+
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
+
+console.log((2.7).toFixed(0)); // always a string
+console.log((2.7).toFixed(3));
+console.log((2.7).toFixed(5));
+
+console.log((2.375).toFixed(0));
+console.log((2.375).toFixed(3));
+console.log(+(2.375).toFixed(5));
